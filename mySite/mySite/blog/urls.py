db0a44cd9a0c0f4877071c39be6843a7177672e8
Gaurav Dhameeja <gdhameeja@gmail.com>
2017-10-23 18:34:11 +0530

    Search structure defined, implementation pending

--- mySite/mySite/blog/urls.py
+++ mySite/mySite/blog/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    #url(r'^search_results/$', search, name="search")
+    url(r'^search_results/$', views.search, name="search")
 ]
--- mySite/mySite/blog/views.py
+++ mySite/mySite/blog/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render_to_response
 
 def index(request):
     return render_to_response("index.html")
+def search(request):
+    return render_to_response("search_results.html")
