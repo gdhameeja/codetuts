27c2bf9033ef1afa601991fce6585af73bc2175a
Gaurav Dhameeja <gdhameeja@gmail.com>
2017-10-23 17:57:41 +0530

    Project Structure
    Corrected project structure

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^/', index, name="index"),
+    url(r'^search_results/$', search, name="search")
 ]
